10e0be4e22b11e36d28f5a5d3969c3476ca9630c
Santiago Eibe <santiago.eibe@upm.es>
2024-07-16 08:16:23 +0000

    Quartz sync: Jul 16, 2024, 8:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🧙 QuartzTest",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "es-ES",
+    baseUrl: "seibe_gitlab.github.io/quartztest",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
